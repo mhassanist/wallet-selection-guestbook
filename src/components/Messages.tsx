aecd709709e404df63260482d2ed2de17d9aafd9
mohammed saudi <m.hassan@ogr.iu.edu.tr>
2022-04-23 23:22:23 +0300

    v1

---
+++ src/components/Messages.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Message } from "../interfaces";
+
+interface MessagesProps {
+  messages: Array<Message>;
+}
+
+const Messages: React.FC<MessagesProps> = ({ messages }) => {
+  return (
+    <>
+      <h2>Messages</h2>
+      {messages.map((message, i) => (
+        <p key={i} className={message.premium ? "is-premium" : ""}>
+          <strong>{message.sender}</strong>:<br />
+          {message.text}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default Messages;
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const SignIn: React.FC = () => {
+  return (
+    <Fragment>
+      <p>Welcome to NEAR Messages Wallet Selector</p>
+      <p>Go ahead and sign in to try it out!</p>
+    </Fragment>
+  );
+};
+
+export default SignIn;
